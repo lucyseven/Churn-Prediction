804c05d34eab5f0aaf12b96829eab3d49dbd6ced
lucyseven <melongreen.lucy@gmail.com>
2022-12-28 15:41:06 -0800

    new main py

--- main.py
+++ main.py
@@ -13,28 +13,22 @@ import pickle
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-file = st.file_uploader('Upload a CSV file')
-# Check if a file was uploaded
-if file is not None:
-    # Read the contents of the file into a DataFrame
-    df = pd.read_csv(file)
-
-    # Display the DataFrame to the user
-    st.write(df)
-
-# Define a function that takes input data and returns the predicted output
-def predict(df):
-    return model.predict(df)
 
 # Add a sidebar to the app
 st.sidebar.title('K-Means Model')
 
-file = st.file_uploader('Upload a CSV file')
+file1 = st.file_uploader('Upload a CSV file' ,key = 'file1')
 
 # Check if a file was uploaded
-if file is not None:
+if file1 is not None:
     # Read the contents of the file into a DataFrame
-    df = pd.read_csv(file)
+    df = pd.read_csv(file1)    
+    
+# Define a function that takes input data and returns the predicted output
+def predict(df):
+    return model.predict(df)
+
+
 
 # Use the model to make a prediction
 prediction = predict(df)
