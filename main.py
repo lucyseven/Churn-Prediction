8043eea0a918ccaf700616dd125260133f12a104
lucyseven <melongreen.lucy@gmail.com>
2022-12-28 16:58:28 -0800

    main again2

--- main.py
+++ main.py
@@ -24,23 +24,19 @@ with open('model.pkl', 'rb') as f:
 st.sidebar.title('K-Means Model')
 
 file1 = st.file_uploader('Upload a CSV file' ,key = 'file1')
-
-# Check if a file was uploaded
+ 
+    
+#Check if a file was uploaded
 if file1 is not None:
     # Read the contents of the file into a DataFrame
-    df = pd.read_csv(file1)    
-    
-# Define a function that takes input data and returns the predicted output
-def predict(df):
-    return model.predict(df)
+    df = pd.read_csv(file1)   
 
 
-
-# Use the model to make a prediction
-prediction = predict(df)
+    # Use the model to make a prediction
+    prediction = model.predict(df)
 
 # Display the prediction to the user
-st.write(prediction)
+    st.write(prediction)
 
 
 # In[ ]:
